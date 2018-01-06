3ff6ee198f6c634b03779b24fa99d300fe2ce9b5
ssinghal16 <soniyasinghal1991@gmail.com>
2018-01-06 18:30:07 +0530

    Assignment2.1 submission

---
+++ MyApplication_Assignment2.1/app/src/main/java/com/example/soniya_singhal/myapplication_assignment21/Assignment2.java
@@ -0,0 +1,13 @@
+package com.example.soniya_singhal.myapplication_assignment21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Assignment2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment2);
+    }
+}
